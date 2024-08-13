229b430b14702240c0fab7d1f0dd83645a87cf46
nghiandd-ds <153247416+nghiandd-ds@users.noreply.github.com>
2024-08-13 13:55:24 +0700

    Create code_explain.py

---
+++ code_explain.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import openai
+from openai import OpenAI
+import numpy as np
+import pandas as pd
+import os
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
+from reportlab.lib import colors
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.enums import TA_LEFT, TA_CENTER
+
+st.write("""
+# Check code
+""")
+st.markdown("LLM can make mistakes. Check important info.")
+
+# Call out OPENAI
+def decoding(encryted_key, password):
+    password = [eval(i) for i in password.split('-')]
+    key = [i for i in encryted_key]
+    combine_key = pd.DataFrame({
+        'index' : password,
+        'encryted_key': key
+    }).sort_values('index')
+    return ''.join(combine_key['encryted_key'])
+
+openai_api_key = decoding('FSeeODhu-tBjpc9j-cM0iJtRRo3rkona7nXEHKk9sWk3bCPI63TrnTlB', '35-21-17-37-41-42-56-47-8-54-16-7-4-10-50-18-3-38-28-55-11-36-45-13-9-19-44-25-39-6-53-43-27-12-40-20-24-14-34-15-1-26-2-30-33-49-46-22-51-23-29-5-48-52-32-31')
+
+# Create two columns, the first one will be used for the input text
+col1, col2 = st.columns([1, 2])
+
+# Add the input text to the left column (col1)
+with col1:
+    user_input = st.text_input("Enter your text here:")
+
+# You can use col2 for any other content you'd like to place on the right side
+with col2:
+    st.write("This is where you can display other content or results.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
