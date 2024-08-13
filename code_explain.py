82525bebc56eec48edce4dc8990d385024c10cbe
nghiandd-ds <153247416+nghiandd-ds@users.noreply.github.com>
2024-08-13 15:54:26 +0700

    Update code_explain.py

--- code_explain.py
+++ code_explain.py
@@ -30,6 +30,7 @@ def decoding(encryted_key, password):
     return ''.join(combine_key['encryted_key'])
 
 openai_api_key = decoding('FSeeODhu-tBjpc9j-cM0iJtRRo3rkona7nXEHKk9sWk3bCPI63TrnTlB', '35-21-17-37-41-42-56-47-8-54-16-7-4-10-50-18-3-38-28-55-11-36-45-13-9-19-44-25-39-6-53-43-27-12-40-20-24-14-34-15-1-26-2-30-33-49-46-22-51-23-29-5-48-52-32-31')
+client = OpenAI(api_key=openai_api_key)
 
 Message = "Given the following code:"
 
