bac25370dc568b990ddc502e40bce5c9b569bf4e
nghiandd-ds <153247416+nghiandd-ds@users.noreply.github.com>
2024-08-19 16:52:38 +0700

    Update code_explain.py

--- code_explain.py
+++ code_explain.py
@@ -5,8 +5,11 @@ import numpy as np
 import pandas as pd
 import os
 from io import BytesIO
+import markdown
+import pdfkit
+
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
+from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
 from reportlab.lib import colors
@@ -91,118 +94,33 @@ def ask(client, mess):
     return text
 #######
 # PDF function
-
-pdfmetrics.registerFont(TTFont('Arial', "arial.ttf"))
-
-def seprate_table(text, sep='|'):
-    first_v = text.find(sep)
-    last_v = text.rfind(sep)+1
-    if first_v != -1 and last_v != 0:
-        return [text[:first_v], text[first_v:last_v], text[last_v:]]
-    else:
-        return [text]
-
-def split_code(code, sep = '|'):
-    case_1 = f"){sep}("
-    case_2 = f") {sep} ("
-    code = code.replace(case_1, '_TOKEN_PLACE_HOLDER_1_')
-    code = code.replace(case_2, '_TOKEN_PLACE_HOLDER_2_')
-    a = code.split(sep)
-    return [org.replace('_TOKEN_PLACE_HOLDER_1_', case_1).replace('_TOKEN_PLACE_HOLDER_2_', case_2) for org in a]
-
-def process_table(text, doc, sep='|'):
-    '''
-    Code to make table form text with a type of separation between columns.
-    Input: 
-        - text: content to make table
-        - sep: how to separate text for table columns
-        - doc: the ouput document that table go to
-    '''
-    list_of_token = [t for t in split_code(text) if (t != '<br/>') and (t.strip() != '') and (t != '/n')]
-    
-    code_part = []
-    code = []
-    exp = []
-    for i in range(0, len(list_of_token)//3):
-        code_part.append(list_of_token[3*i])
-        code.append(list_of_token[1 + 3*i])
-        exp.append(list_of_token[2 + 3*i])
-
-    # Font style
-    header_style = ParagraphStyle(
-            'HeaderStyle',
-            fontName='Arial',
-            fontSize=12,
-            textColor=colors.whitesmoke,
-            alignment=TA_CENTER,
-            spaceAfter=6
-        )
-        
-    body_style = ParagraphStyle(
-            'BodyStyle',
-            fontName='Arial',
-            fontSize=10,
-            leading=10,
-            alignment=TA_LEFT
-        )
-    
-    # Create data for table
-    formatted_data = [
-            [Paragraph(cell, header_style) for cell in np.array([code_part, code, exp]).T.tolist()[0]]] + [
-            [Paragraph(cell, body_style) for cell in row] for row in np.array([code_part, code, exp]).T.tolist()[1:]]
-    
-    # Create the table
-    available_width = A4[0] - doc.leftMargin - doc.rightMargin    
-    # Create the table with adjusted column widths
-    col_widths = [available_width * 0.2, available_width * 0.3, available_width * 0.5]
-    table = Table(formatted_data, colWidths=col_widths)
-    # Add style to the table
-    style = TableStyle([
-        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-        ('GRID', (0, 0), (-1, -1), 0.5, colors.black),
-        ('VALIGN', (0, 0), (-1, -1), 'TOP'),
-        ('LEFTPADDING', (0, 0), (-1, -1), 3),
-        ('RIGHTPADDING', (0, 0), (-1, -1), 3),
-        ('TOPPADDING', (0, 0), (-1, -1), 3),
-        ('BOTTOMPADDING', (0, 0), (-1, -1), 6),
-    ])    
-    table.setStyle(style)
-    return table
-
-def make_pdf(text):
+def convert_markdown_to_pdf(markdown_text):
+    # Create a BytesIO buffer
     buffer = BytesIO()
+
+    # Create the PDF object using ReportLab
+    doc = SimpleDocTemplate(buffer, pagesize=letter)
     styles = getSampleStyleSheet()
-    doc = SimpleDocTemplate(buffer, pagesize=A4,
-                                rightMargin=72, leftMargin=72,
-                                topMargin=72, bottomMargin=18)
-    text_ = '<br/><br/>'.join([i.replace('\n', '<br/>').replace('<br>', '<br/>') for i in text])
-    normal_style = ParagraphStyle(
-            'NormalStyle',
-            fontName='Arial',
-            fontSize=10,
-            leading=12
-        )
-    header_style = ParagraphStyle(
-                'HeaderStyle',
-                fontName='Arial',
-                fontSize=20,
-                alignment=TA_CENTER,
-                spaceAfter=36)
-    doc.title = "Code Quality Report"
-    format_text = []
-    align_text = seprate_table(text_, sep='|')
-    if len(align_text) == 3:
-        align_text =[Paragraph('Code Quality Report',  header_style),
-                    Paragraph(align_text[0], normal_style),
-                    process_table(align_text[1], doc, sep='|'),
-                    Paragraph(align_text[2], normal_style)]
-    else:
-        align_text = [Paragraph('Code Quality Report',  header_style), Paragraph(align_text[0], normal_style)]
     
-    doc.build(align_text)
-    buffer.seek(0)
-    return buffer
+    # Split the markdown text into paragraphs
+    paragraphs = []
+    for i in markdown_text:
+        paragraphs = paragraphs.append(st.markdown(i))
+    
+    # Convert markdown paragraphs to ReportLab Paragraph objects
+    elements = []
+    for para in paragraphs:
+        p = Paragraph(para, styles['Normal'])
+        elements.append(p)
+    
+    # Build the PDF
+    doc.build(elements)
+    
+    # Get the value of the BytesIO buffer
+    pdf = buffer.getvalue()
+    buffer.close()
+    
+    return pdf
 
 
 
@@ -264,7 +182,7 @@ with col_2:
         query_ = Message + "/n/n" + user_input  + "/n/n" + explain_code_query
         text = ask(client, query_)
         
-        buffer = make_pdf(text)
+        buffer = convert_markdown_to_pdf(text)
         @st.experimental_fragment
         def download_file():
             st.download_button(
@@ -283,7 +201,7 @@ with col_2:
         query_ = Message + "/n/n" + user_input  + "/n/n" + add_comments
         text = ask(client, query_)
         
-        buffer = make_pdf(text)
+        buffer = convert_markdown_to_pdf(text)
         @st.experimental_fragment
         def download_file():
             st.download_button(
@@ -302,7 +220,7 @@ with col_2:
         query_ = Message + "/n/n" + user_input  + "/n/n" + optimization
         text = ask(client, query_)
         
-        buffer = make_pdf(text)
+        buffer = convert_markdown_to_pdf(text)
         @st.experimental_fragment
         def download_file():
             st.download_button(
