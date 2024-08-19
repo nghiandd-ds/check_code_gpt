55fecdbcbd734f7e1a9dfdc5b83771c184704499
nghiandd-ds <153247416+nghiandd-ds@users.noreply.github.com>
2024-08-19 17:16:58 +0700

    Update code_explain.py

--- code_explain.py
+++ code_explain.py
@@ -97,7 +97,7 @@ def ask(client, mess):
 def convert_markdown_to_pdf(markdown_text):
     html_content = "<html><body>"
     for section in markdown_text:
-        html_content += st.markdown(markdown_text).html_output
+        html_content += st.markdown(section).html_output
         html_content += "<hr>"
     html_content += "</body></html>"
 
