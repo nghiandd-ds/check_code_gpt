ec6b0b8671a3e20cc9367c8900e15a6123b153e7
nghiandd-ds <153247416+nghiandd-ds@users.noreply.github.com>
2024-08-20 13:43:52 +0700

    Update code_explain.py

--- code_explain.py
+++ code_explain.py
@@ -196,15 +196,13 @@ with col_2:
     if user_input and logic_button:
         @st.experimental_fragment
         def check_code():
-            st.download_button(
-                code_purpose = st.text_area("Describe code's purpose", height=150)
-                submit_logic = st.button('Check')
-                if code_purpose and submit_logic:
-                    query_ = Message + "/n/n" + user_input  + "/n/n" + optimization + '/n' + "Purpose: " + code_purpose
-                    text = ask(client, query_)
-                    st.markdown('/n/n'.join(text))
-                    st.stop()
-                )
+            code_purpose = st.text_area("Describe code's purpose", height=150)
+            submit_logic = st.button('Check')
+            if code_purpose and submit_logic:
+                query_ = Message + "/n/n" + user_input  + "/n/n" + optimization + '/n' + "Purpose: " + code_purpose
+                text = ask(client, query_)
+                st.markdown('/n/n'.join(text))
+                st.stop()
         check_code()
 
 st.stop()
