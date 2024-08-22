6deea034e024c5d225b00e77aa164cf7d0888702
nghiandd-ds <153247416+nghiandd-ds@users.noreply.github.com>
2024-08-22 17:15:09 +0700

    Update code_explain.py

--- code_explain.py
+++ code_explain.py
@@ -158,8 +158,8 @@ with col_2:
         @st.experimental_fragment
         def logic_checker():
             code_purpose = st.text_input("Describe code's purpose/logic", key="code_purpose", 
-                                         value=st.session_state.code_purpose, 
-                                         placeholder=st.session_state.code_purpose)
+                                         value=None, 
+                                         placeholder='')
             st.markdown("<b>IMPORTANT</b>: This is a alpha version. Some of the intented function might not work!", unsafe_allow_html=True)
             query_ = Message + "/n/n" + st.session_state.user_input + "/n/n" + logic_code + '/n/n' + "Purpose/Logic: " +  code_purpose
             if code_purpose:
